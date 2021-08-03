cd011f7c471ecb81996a8970302885e3dab5f346
Tymur <75130519+temarych@users.noreply.github.com>
2021-08-04 00:36:39 +0300

    Refactor script.js

--- Part 3 - To Do List/script.js
+++ Part 3 - To Do List/script.js
@@ -1,25 +1,33 @@
 let add = document.querySelector("#add");
 let list = document.querySelector("#list");
-add.onclick = (e) => {
-    e.preventDefault();
-    let listitem = document.querySelector("#listitem");
 
-    if (listitem.value !== '') {
-        let el = document.createElement("li");
-        el.innerHTML = listitem.value;
-        let removebtn = document.createElement("button");
-        removebtn.className = "close";
-        removebtn.innerHTML = "\u00D7";
-        el.appendChild(removebtn);
-        list.appendChild(el);
-        listitem.value = "";
-        el.addEventListener("click", (e) => {
-            e.target.style.textDecoration = "line-through";
-        })
-        removebtn.addEventListener("click", (e) => {
-            e.target.parentElement.style.display = "none";
-        });
-    } else {
-        alert("No input\nWrite something!!");
+add.onclick = event => {
+    event.preventDefault();
+    
+    let listitem = document.querySelector("#listitem");
+    
+    if (!listitem.value.length) {
+        return alert("No input\nWrite something!!");
     }
+
+    let li = document.createElement("li");
+    let button = document.createElement("button");
+    
+    li.innerHTML = listitem.value;
+    
+    button.className = "close";
+    button.innerHTML = "\u00D7";
+    
+    li.appendChild(button);
+    listitem.appendChild(li);
+    
+    listitem.value = "";
+    
+    li.onclick = event => {
+        event.target.style.textDecoration = "line-through";
+    };
+    
+    button.onclick = event => {
+        event.target.parentElement.style.display = "none";
+    };
 }
