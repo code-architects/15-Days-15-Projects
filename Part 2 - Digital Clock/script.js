ea1dbf2fb0534a4fda6fb34f126df99d9565aa06
Tymur <75130519+temarych@users.noreply.github.com>
2021-08-04 00:17:56 +0300

    Refactor script.js

--- Part 2 - Digital Clock/script.js
+++ Part 2 - Digital Clock/script.js
@@ -3,41 +3,28 @@ window.onload = function () {
 };
 
 function updateTime() {
-	const hourEL = document.querySelector("#hour");
-	const minuteEL = document.querySelector("#minute");
-	const secondEL = document.querySelector("#second");
-	let d = new Date();
-	let hours = d.getHours();
-	let minutes = d.getMinutes();
-	let seconds = d.getSeconds();
-	if (hours >= 0 && hours <= 9) hours = "0" + hours;
-	if (minutes >= 0 && minutes <= 9) minutes = "0" + minutes;
-	if (seconds >= 0 && seconds <= 9) seconds = "0" + seconds;
-	hourEL.innerHTML = hours;
-	minuteEL.innerHTML = minutes;
-	secondEL.innerHTML = seconds;
+	const format = number => `${ +!(number < 10) && '' }${number}`;
+	
+	let date = new Date();
+	
+	document.querySelector("#hour")  .innerHTML = format( date.getHours()   );
+	document.querySelector("#minute").innerHTML = format( date.getMinutes() );
+	document.querySelector("#second").innerHTML = format( date.getSeconds() );
 
-
-	const dateEl = document.querySelector("#date");
-	let date = d.getDate();
-	if (date >= 0 && date <= 9) date = "0" + date;
-	let month = d.getMonth() + 1;
-	if (month >= 0 && month <= 9) month = "0" + month;
-	let year = d.getFullYear();
-	if (year >= 0 && year <= 9) year = "0" + year;
-	dateEl.innerHTML = `${date}/${month}/${year}`;
-
-
-	const dayEL = document.querySelector("#day");
-	let daynumber = d.getDay(), day = "";
-	switch (daynumber) {
-		case 0: day = "Sunday"; break;
-		case 1: day = "Monday"; break;
-		case 2: day = "Tuesday"; break;
-		case 3: day = "Wednesday"; break;
-		case 4: day = "Thrusday"; break;
-		case 5: day = "Friday"; break;
-		case 6: day = "Saturday"; break;
-	}
-	dayEL.innerHTML = day;
+	document.querySelector("#date").innerHTML = (
+		[
+			date.getDate(), 
+			++date.getMonth(), 
+			date.getFullYear()
+		]
+			.map(format)
+			.join('/');
+	);
+	
+	const week = [
+		"Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"
+	];
+	
+	document.querySelector("#day").innerHTML = week[ date.getDay() ];
 }
